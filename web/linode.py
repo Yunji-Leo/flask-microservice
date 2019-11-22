63a7de105d59d58f4d1290075bf912bd43fb60cc
Leo Zhong <yunji@uber.com>
2019-11-21 22:57:11 -0800

    fix error

--- web/linode.py
+++ web/linode.py
@@ -42,12 +42,12 @@ def hello_world():
         count = (cache.get('visitor_count')).decode('utf-8')
         update = PgFetch("UPDATE visitors set visitor_count = " + count + " where site_id = 1;", "POST")
     else:
-        cache_refresh = PgFetch("SELECT visitor_count FROM visitor where site_id = 1;", "GET")
+        cache_refresh = PgFetch("SELECT visitor_count FROM visitors where site_id = 1;", "GET")
         count = int(cache_refresh[0])
         cache.set('visitor_count', count)
         cache.incr('visitor_count')
         count = (cache.get('visitor_count')).decode('utf-8')
-    return 'Hello Linode! This page has been viewed %s time(s).' % count
+    return 'Hello Linode!  This page has been viewed %s time(s).' % count
 
 @app.route('/resetcounter')
 def resetcounter():
@@ -55,4 +55,3 @@ def resetcounter():
     PgFetch("UPDATE visitors set visitor_count = 0 where site_id = 1;", "POST")
     app.logger.debug("reset visitor count")
     return "Successfully deleted redis and postgres counters"
-
